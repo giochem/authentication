d02afd5d41a955c385b26e869a7660835b55e694
truong <tranxuantruong203hn@gmail.com>
2024-11-16 00:22:36 +0700

    jwt refresh token automatic

--- server.js
+++ server.js
@@ -1,21 +1,73 @@
+require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
+
+const signAccessToken = async () => {
+  const payload = {
+    userId: 1,
+    username: "admin",
+  };
+
+  const token = await jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15s",
+  });
+
+  return token;
+};
+const signRefreshToken = async () => {
+  const payload = {
+    userId: 1,
+    username: "admin",
+  };
+
+  const token = await jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "10m",
+  });
+
+  return token;
+};
+const verifyToken = async (req, res, next) => {
+  try {
+    if (req.headers["x-token"]) {
+      const token = req.headers["x-token"];
+      const payload = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      req.user = payload;
+      return next();
+    }
+
+    return res.status(200).json({
+      code: 401,
+      msg: "Invalid token",
+    });
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(200).json({
+        code: 401,
+        msg: error.message,
+      });
+    }
+    return res.status(200).json({
+      code: 500,
+      msg: error,
+    });
+  }
+};
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/refreshToken", (req, res) => {
+app.get("/api/refreshToken", async (req, res) => {
   return res.status(200).json({
     status: "success",
     elements: {
-      token: "newAccessToken",
-      timeExpired: Date.now() + 3 * 1000,
+      accessToken: await signAccessToken(),
     },
   });
 });
 
-app.get("/api/users", (req, res) => {
+app.get("/api/users", verifyToken, (req, res) => {
   return res.status(200).json({
     status: "success",
     elements: [
@@ -29,12 +81,11 @@ app.get("/api/users", (req, res) => {
   });
 });
 
-app.get("/api/login", (req, res) => {
+app.get("/api/login", async (req, res) => {
   return res.status(200).json({
     status: "success",
     elements: {
-      token: "access",
-      timeExpired: Date.now() + 3 * 1000,
+      accessToken: await signAccessToken(),
     },
   });
 });
