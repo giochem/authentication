e6cc166926436f4d9b3f5e8e69e2e335384496ca
Xuân Trường <96410595+giochem@users.noreply.github.com>
2024-11-16 00:24:52 +0700

    jwt using accesstoken and refreshtoken basic (#2)

--- server.js
+++ server.js
@@ -1,66 +1,42 @@
 const express = require("express");
-const sessions = require("express-session");
-
 const app = express();
 
-app.use(
-  sessions({
-    secret: "session secret",
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 24 hours
-    },
-    resave: true,
-    saveUninitialized: false,
-  })
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
-  if (!req.session.userid) {
-    return res.redirect("/login");
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write(`
-      <h1>Welcome back ${req.session.userid}</h1>
-      <a href="/logout">Logout</a>
-    `);
-
-  res.end();
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/login", (req, res) => {
-  if (req.session.userid) {
-    return res.redirect("/");
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write(`
-      <h1>Login</h1>
-      <form method="post" action="/process-login">
-        <input type="text" name="username" placeholder="Username" /> <br>
-        <input type="password" name="password" placeholder="Password" /> <br>
-        <button type="submit">Login</button>
-      </form>
-    `);
-
-  res.end();
+app.get("/api/refreshToken", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+    elements: {
+      token: "newAccessToken",
+      timeExpired: Date.now() + 3 * 1000,
+    },
+  });
 });
 
-app.post("/process-login", (req, res) => {
-  if (req.body.username !== "admin" || req.body.password !== "admin") {
-    return res.send("Invalid username or password");
-  }
-
-  req.session.userid = req.body.username;
-
-  res.redirect("/");
+app.get("/api/users", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+    elements: [
+      {
+        name: "John",
+      },
+      {
+        name: "Jane",
+      },
+    ],
+  });
 });
 
-app.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.redirect("/");
+app.get("/api/login", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+    elements: {
+      token: "access",
+      timeExpired: Date.now() + 3 * 1000,
+    },
+  });
 });
 
 const PORT = 3000;
