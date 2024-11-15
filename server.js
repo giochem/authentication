e07dd41c859c6fed543ea72bf3811b588858585d
truong <tranxuantruong203hn@gmail.com>
2024-11-15 21:26:11 +0700

    using cookie, session

--- server.js
+++ server.js
@@ -1,35 +1,69 @@
 const express = require("express");
-const base64 = require("base-64");
+const sessions = require("express-session");
+
 const app = express();
-const PORT = 3000;
 
-function decodeCredentials(authHeader) {
-  // authHeader: Basic YWRtaW46YWRtaW4=
-  const encodedCredentials = authHeader.trim().replace(/Basic\s+/i, "");
+app.use(
+  sessions({
+    secret: "session secret",
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    },
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
-  const decodedCredentials = base64.decode(encodedCredentials);
-
-  return decodedCredentials.split(":");
-}
-
-function authMiddleware(req, res, next) {
-  const [username, password] = decodeCredentials(
-    req.headers.authorization || ""
-  );
-
-  if (username === "admin" && password === "admin") {
-    return next();
-  }
-
-  res.set("WWW-Authenticate", 'Basic realm="user_pages"');
-  res.status(401).send("Authentication required.");
-}
-
-app.use(authMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Authenticated !!!");
+  if (!req.session.userid) {
+    return res.redirect("/login");
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(`
+      <h1>Welcome back ${req.session.userid}</h1>
+      <a href="/logout">Logout</a>
+    `);
+
+  res.end();
 });
+
+app.get("/login", (req, res) => {
+  if (req.session.userid) {
+    return res.redirect("/");
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(`
+      <h1>Login</h1>
+      <form method="post" action="/process-login">
+        <input type="text" name="username" placeholder="Username" /> <br>
+        <input type="password" name="password" placeholder="Password" /> <br>
+        <button type="submit">Login</button>
+      </form>
+    `);
+
+  res.end();
+});
+
+app.post("/process-login", (req, res) => {
+  if (req.body.username !== "admin" || req.body.password !== "admin") {
+    return res.send("Invalid username or password");
+  }
+
+  req.session.userid = req.body.username;
+
+  res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running @ http://localhost:${PORT}`);
